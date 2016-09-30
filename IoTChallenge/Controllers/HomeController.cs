2e5dff0fdec5310bfeeb6795a62067bd67652c62
Tim <tprovance@gmail.com>
2016-09-30 05:49:22 -0700

    The latest with super cool automatic SNS subscription.

--- IoTChallenge/Controllers/HomeController.cs
+++ IoTChallenge/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using IoTChallenge.Hubs;
 using IoTChallenge.Models;
 using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace IoTChallenge.Controllers
@@ -24,12 +26,36 @@ namespace IoTChallenge.Controllers
             return View(results);
         }
 
-        public void Refresh(string message)
+        [System.Web.Mvc.HttpPost]
+        public void Refresh()
         {
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<DefconHub>();
             var model = new DefconModel();
             var results = model.GetRoomInfo();
             context.Clients.All.loadResults(results);
+
+            var body = new System.IO.StreamReader(HttpContext.Request.InputStream).ReadToEnd();
+            var message = JsonConvert.DeserializeObject<SnsMessage>(body);
+
+            if (null != message && !string.IsNullOrWhiteSpace(message.SubscribeURL))
+            {
+                context.Clients.All.addNewMessageToPage("API", "Confirming SNS Subscription");
+                ExternalWebRequests.ExternalHttpRequest(message.SubscribeURL, Common.HttpVerbs.GET);
+            }
+        }
+
+        public class SnsMessage
+        {
+            public string Type { get; set; }
+            public string MessageId { get; set; }
+            public string Token { get; set; }
+            public string TopicArn { get; set; }
+            public string Message { get; set; }
+            public string SubscribeURL { get; set; }
+            public string Timestamp { get; set; }
+            public string SignatureVersion { get; set; }
+            public string Signature { get; set; }
+            public string SigningCertURL { get; set; }
         }
 
     }   
